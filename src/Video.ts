8c67a97ad5321e4fb50f55118acbe219a4fcfbda
Retro Indie Josh <mrjoshuamclean@gmail.com>
2022-08-01 08:40:37 -0400

    Cleanup structure and makefile

--- src/Video.ts
+++ src/Video.ts
@@ -7,6 +7,8 @@ class Video
         private height: number = 64;
         private scale: number = 4;
 
+        private colors: Array<string> | null = null;
+
         constructor(canvas_id: string, width: number, height: number, scale: number) {
                 this.canvas = document.getElementById(canvas_id) as HTMLCanvasElement;
                 if (this.canvas == null) {
@@ -25,18 +27,26 @@ class Video
                 this.canvas.width = width * scale;
                 this.canvas.height = height * scale;
 
-                var pixels = new Array(width);
-                for (var x = 0; x < width; ++x) {
+                let pixels = new Array(width);
+                for (let x = 0; x < width; ++x) {
                         pixels[x] = new Array(height);
                 }
         }
 
         draw() {
-                for (var x = 0; x < this.width; ++x) {
-                        for (var y = 0; y < this.height; ++y) {
+                for (let x = 0; x < this.width; ++x) {
+                        for (let y = 0; y < this.height; ++y) {
                                 this.ctx.fillStyle = random_color();
                                 this.ctx.fillRect(x * this.scale, y * this.scale, this.scale, this.scale);
                         }
                 }
         }
 } 
+
+function random_color(): string
+{
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+        return `rgb(${r}, ${g}, ${b}`;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,5 @@
-function draw()
+function retconjs_init(): void
 {
         const video = new Video('retcon', 64, 64, 4);
         video.draw();
-}
-
-function random_color()
-{
-        let r = Math.floor(Math.random() * 255);
-        let g = Math.floor(Math.random() * 255);
-        let b = Math.floor(Math.random() * 255);
-        return `rgb(${r}, ${g}, ${b}`;
 }
