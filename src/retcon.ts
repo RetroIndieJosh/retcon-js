8fc12f5a11704408a6db3f29e227c34a1fbef603
Retro Indie Josh <mrjoshuamclean@gmail.com>
2022-08-02 12:06:33 -0400

    Load game data

--- src/retcon.ts
+++ src/retcon.ts
@@ -1,10 +1,41 @@
+class IdDataBase {
+        public id: number = -1;
+}
+
+class SizedDataBase extends IdDataBase {
+        public width: number = -1;
+        public height: number = -1;
+}
+
+class TileDataBase extends SizedDataBase {
+        public tiles: string = "";
+}
+
+class TileData extends IdDataBase {
+        public pixels: string = "";
+}
+
+class TilesetData extends TileDataBase { }
+
+class TilemapData extends TileDataBase {
+        public palette: number = -1;
+        public tileset: number = -1;
+}
+
+class ActorData extends TileDataBase { }
+
+class PaletteData extends IdDataBase {
+        public color_count: number = -1;
+        public colors: string = "";
+
+}
 // TODO write separate software (Python?) compiler that converts .map, .set, .tile, and .pal files into json, then retcon reads that json file
 class GameData {
         public title: string = "";
-        public palettes: Array<string> = new Array<string>();
-        public tiles: Array<string> = new Array<string>();
-        public tilesets: Array<string> = new Array<string>();
-        public tilemaps: Array<string> = new Array<string>();
+        public palettes: Array<PaletteData> = new Array<PaletteData>();
+        public tiles: Array<TileData> = new Array<TileData>();
+        public tilesets: Array<TilesetData> = new Array<TilesetData>();
+        public tilemaps: Array<TilemapData> = new Array<TilemapData>();
 }
 
 function retconjs_init(scale: number, debug: boolean = false): void {
