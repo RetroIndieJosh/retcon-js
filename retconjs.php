223a4f41a2f6798040844d9920a96e4bd96b2195
Retro Indie Josh <mrjoshuamclean@gmail.com>
2022-08-02 09:20:45 -0400

    Update retcon include php to inform when files missing

--- retconjs.php
+++ retconjs.php
@@ -1,5 +1,22 @@
-<script src="Color.js"></script>
-<script src="Surface.js"></script>
-<script src="Video.js"></script>
-<script src="Sprite.js"></script>
-<script src="retcon.js"></script>
+<?php
+
+function add_js(string $filename): bool {
+        if(file_exists($filename)) {
+                echo "<script src='$filename'></script>";
+                return true;
+        }
+
+        echo "<div>Missing required JS file: '$filename'</div>";
+        return false;
+}
+
+$tsconfig_data = file_get_contents('src/tsconfig.json');
+$tsconfig = json_decode($tsconfig_data, true);
+
+// TODO read from src/tsconfig.json "files"
+$file_list = $tsconfig["files"];
+
+$success = true;
+foreach($file_list as $file) {
+        $success = $success && add_js(str_replace(".ts", ".js", $file));
+}
